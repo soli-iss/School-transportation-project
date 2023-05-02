41149048d9f3e5ecfacdc85976f6d7c9516b0652
Soli Iss <soli8444@gmail.com>
2023-05-02 21:37:54 +0300

    try 1

---
+++ demo/src/main/java/app/core/SchoolTransportationProjectApplication.java
@@ -0,0 +1,14 @@
+package app.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolTransportationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolTransportationProjectApplication.class, args);
+		System.out.println("started");
+	}
+
+}
---
+++ demo/src/main/java/app/core/entities/Teachers.java
@@ -0,0 +1,36 @@
+package app.core.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Teachers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(unique = true,nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private int phoneNumber;
+
+    @Column(nullable = false)
+    private int classNumber ; //?
+
+
+
+}
